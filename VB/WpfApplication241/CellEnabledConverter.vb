55e2e0ba46f981e9d0ad8a9bdfe7f2e376d7b985
DevExpressExampleBot <fakeEmail>
2021-09-06 19:24:01 +0300

    Source auto update [skip ci]

--- VB/WpfApplication241/CellEnabledConverter.vb
+++ VB/WpfApplication241/CellEnabledConverter.vb
@@ -6,27 +6,27 @@ Imports System.Windows.Data
 Imports System.Windows.Markup
 
 Namespace WpfApplication241
-    Public Class CellEnabledConverter
-        Inherits MarkupExtension
-        Implements IMultiValueConverter
+	Public Class CellEnabledConverter
+		Inherits MarkupExtension
+		Implements IMultiValueConverter
 
-        Public Function Convert(ByVal values() As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IMultiValueConverter.Convert
-            Dim disabledDates As List(Of Date) = TryCast(values(0), List(Of Date))
-            Dim cellButton As Button = DirectCast(values(1), Button)
-            Dim dt As Date = CDate(DateEditCalendar.GetDateTime(cellButton))
-            If disabledDates IsNot Nothing Then
-                Return disabledDates.Contains(dt)
-            Else
-                Return False
-            End If
-        End Function
+		Public Function Convert(ByVal values() As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IMultiValueConverter.Convert
+			Dim disabledDates As List(Of DateTime) = TryCast(values(0), List(Of DateTime))
+			Dim cellButton As Button = DirectCast(values(1), Button)
+			Dim dt As DateTime = CDate(DateEditCalendar.GetDateTime(cellButton))
+			If disabledDates IsNot Nothing Then
+				Return disabledDates.Contains(dt)
+			Else
+				Return False
+			End If
+		End Function
 
-        Public Function ConvertBack(ByVal value As Object, ByVal targetTypes() As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object() Implements IMultiValueConverter.ConvertBack
-            Throw New NotImplementedException()
-        End Function
+		Public Function ConvertBack(ByVal value As Object, ByVal targetTypes() As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object() Implements IMultiValueConverter.ConvertBack
+			Throw New NotImplementedException()
+		End Function
 
-        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-            Return Me
-        End Function
-    End Class
+		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+			Return Me
+		End Function
+	End Class
 End Namespace
--- VB/WpfApplication241/DateEditCalendarEx.vb
+++ VB/WpfApplication241/DateEditCalendarEx.vb
@@ -7,15 +7,15 @@ Imports System.Windows
 Imports System.Windows.Controls
 
 Namespace WpfApplication241
-    Public Class DateEditCalendarEx
-        Inherits DateEditCalendar
+	Public Class DateEditCalendarEx
+		Inherits DateEditCalendar
 
-        Protected Overrides Sub OnCellButtonClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Dim button = TryCast(sender, Button)
-            If button.IsEnabled Then
-                MyBase.OnCellButtonClick(sender, e)
-            End If
+		Protected Overrides Sub OnCellButtonClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim button As Button = TryCast(sender, Button)
+			If button.IsEnabled Then
+				MyBase.OnCellButtonClick(sender, e)
+			End If
 
-        End Sub
-    End Class
+		End Sub
+	End Class
 End Namespace
